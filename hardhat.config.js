34b6d083a94a95d7da050daf0e566dd0174e81c2
root <root@DESKTOP-JOHV3EV.localdomain>
2021-11-17 16:07:28 +0000

    Commit git page

---
+++ hardhat.config.js
@@ -0,0 +1,33 @@
+require("@nomiclabs/hardhat-waffle");
+
+// This is a sample Hardhat task. To learn how to create your own go to
+// https://hardhat.org/guides/create-task.html
+task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+  const accounts = await hre.ethers.getSigners();
+
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+});
+
+// You need to export an object to set up your config
+// Go to https://hardhat.org/config/ to learn more
+
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+module.exports = {
+  solidity: "0.8.4",
+  paths: {
+    artifacts: './src/artifacts',
+  },
+  networks: {
+    hardhat: {
+      chainId: 1337
+    },
+    ropsten: {
+      url: "https://ropsten.infura.io/v3/8175898b95384f0781f755af5b23761d",
+      accounts: [`677defea7f9884c6ad2f4e3c1fab515685b56d90fc2b1de3c38afc7ca5d54ed1`]
+    }
+  }
+};
