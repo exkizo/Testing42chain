bd55c8b78fd5a21842408e4bdec919fbb8f0b972
root <root@DESKTOP-JOHV3EV.localdomain>
2021-11-20 18:43:38 +0000

    changing thins

--- src/App.js
+++ src/App.js
@@ -3,7 +3,31 @@ import { ethers, providers } from 'ethers';
 import './App.css';
 import Greeter from './artifacts/contracts/Greeter.sol/Greeter.json'
 
-const greeterAddress = "0xcE17f206b3d520C254FaB31d749AB75Fe481d8D4"
+const greeterAddress = "0x0dFCaDd0C1D0FD8E0c82E829Db0D78d9447a2F9A"
+
+function Header() {
+  return (<h3>The goal of this Dapp in to write and read from the Ropsten Testnet Blockchain!</h3>)
+}
+
+function Instructions() {
+  return (<div><p>For you to be able to use this aplication, you will need a Metamask connected to the Ropsten Tesnet and some fake ETH:</p></div>)
+}
+
+function TableInstructions() {
+  return (
+  <table>
+    <tbody>
+    <tr>
+      <td>Donwload Metamask:</td>
+      <td><a href="https://metamask.io/" target="_blank">Metamask Official Website</a></td>
+    </tr>
+    <tr>
+      <td>Get some fake ETH:</td>
+      <td><a href="https://faucet.ropsten.be/" target="_blank">Ropsten ETH faucet</a></td>
+    </tr>
+    </tbody>
+  </table>)
+}
 
 function App() {
   const [greeting, setGreetingValue] = useState('')
@@ -32,23 +56,26 @@ function App() {
       const contract = new ethers.Contract(greeterAddress, Greeter.abi, provider)
       try {
         const data = await contract.greet()
-        console.log ('data: ', data)
+        return(alert(data))
       } catch (err) {
         console.log ("Error: ", err)
       }
     }
   }
-
+  
   return (
     <div className="App">
       <header className="App-header">
-        <button onClick={fetchGreeting}>Fetch Greeting</button>
-        <button onClick={setGreeting}>Set Greeting</button>
-        <input
+        <Header />
+        <Instructions />
+        <TableInstructions />
+        <div><button onClick={fetchGreeting}>Read String</button></div>
+        <div><button onClick={setGreeting}>Write String</button></div>
+        <div><input
           onChange={e => setGreetingValue(e.target.value)}
-          placeholder="Set greeting"
+          placeholder="New string"
           value={greeting} 
-        />
+        /></div>
       </header>
     </div>
   );
